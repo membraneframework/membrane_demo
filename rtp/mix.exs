93e34655c525abd1dab23d7eb90f8044a2742241
Dominik Stanaszek <dominik.stanaszek@swmansion.com>
2019-11-19 18:13:47 +0100

    Update core

--- rtp/mix.exs
+++ rtp/mix.exs
@@ -24,7 +24,7 @@ defmodule Membrane.Demo.RTP.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:membrane_core, [env: :prod, path: "~/membrane-core", override: true]},
+      {:membrane_core, "0.5.0"},
       {:membrane_bin_rtp, "0.1.0"},
       {:membrane_element_udp, "~> 0.3.0"},
       {:membrane_element_ffmpeg_h264, "~> 0.2.0"},

09b33fa9596b69ef4a7a05968978c3d6a8c1bb0c
Dominik Stanaszek <dominik.stanaszek@swmansion.com>
2019-11-19 18:11:04 +0100

    Update deps

--- rtp/mix.exs
+++ rtp/mix.exs
@@ -5,7 +5,7 @@ defmodule Membrane.Demo.RTP.MixProject do
     [
       app: :membrane_demo_rtp,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -25,12 +25,12 @@ defmodule Membrane.Demo.RTP.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:membrane_core, [env: :prod, path: "~/membrane-core", override: true]},
-      {:membrane_bin_rtp, path: "~/membrane_bin_rtp"},
-      {:membrane_element_udp, "~> 0.2"},
-      {:membrane_element_ffmpeg_h264, "~> 0.1"},
-      {:membrane_element_rtp_h264, "~> 0.1.0"},
-      {:membrane_element_sdl, "~> 0.2.0"},
-      {:membrane_element_portaudio, "~> 0.2.3"}
+      {:membrane_bin_rtp, "0.1.0"},
+      {:membrane_element_udp, "~> 0.3.0"},
+      {:membrane_element_ffmpeg_h264, "~> 0.2.0"},
+      {:membrane_element_rtp_h264, "~> 0.2.0"},
+      {:membrane_element_sdl, "~> 0.3.0"},
+      {:membrane_element_portaudio, "~> 0.3.0"}
     ]
   end
 end

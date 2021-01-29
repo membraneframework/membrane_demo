79c5e76de84213b1f39817628636071e2449066e
Mateusz Front <mateusz.front@swmansion.com>
2021-01-29 18:19:03 +0100

    improve sdp generation

--- webrtc/videoroom/lib/videoroom/stream/sdp_utils.ex
+++ webrtc/videoroom/lib/videoroom/stream/sdp_utils.ex
@@ -41,114 +41,69 @@ defmodule VideoRoom.Stream.SDPUtils do
     %ExSDP{sdp | media: media} |> ExSDP.serialize()
   end
 
-  defp get_example_offer_sdp() do
+  def get_example_offer_sdp() do
+    streams = Enum.to_list(0..3)
+
     """
     v=0
     o=- 7263753815578774817 2 IN IP4 127.0.0.1
     s=-
     t=0 0
-    a=group:BUNDLE audio1 audio2 audio3 video1 video2 video3
+    a=group:BUNDLE #{Enum.map_join(streams, " ", &"audio#{&1}")} #{
+      Enum.map_join(streams, " ", &"video#{&1}")
+    }
     a=msid-semantic: WMS *
-    m=audio 9 UDP/TLS/RTP/SAVPF 120
-    c=IN IP4 0.0.0.0
-    a=rtcp:9 IN IP4 0.0.0.0
-    a=ice-ufrag:1PSY
-    a=ice-pwd:ejBMY08jZ4EWoJbIfuJsgRIS
-    a=ice-options:trickle
-    a=fingerprint:sha-256 24:2D:06:61:0E:59:54:0E:69:08:A4:9F:0A:D9:17:4B:89:50:11:A2:20:65:68:0B:61:11:51:57:EA:F6:11:E4
-    a=setup:actpass
-    a=mid:audio1
-    a=sendrecv
-    a=msid:stream1 stream1-audio
-    a=rtcp-mux
-    a=rtpmap:120 opus/48000/2
-    a=fmtp:120 minptime=10;useinbandfec=1
-    a=ssrc:110 cname:stream1
-    m=audio 9 UDP/TLS/RTP/SAVPF 120
-    c=IN IP4 0.0.0.0
-    a=rtcp:9 IN IP4 0.0.0.0
-    a=ice-ufrag:1PSY
-    a=ice-pwd:ejBMY08jZ4EWoJbIfuJsgRIS
-    a=ice-options:trickle
-    a=fingerprint:sha-256 24:2D:06:61:0E:59:54:0E:69:08:A4:9F:0A:D9:17:4B:89:50:11:A2:20:65:68:0B:61:11:51:57:EA:F6:11:E4
-    a=setup:actpass
-    a=mid:audio2
-    a=sendonly
-    a=msid:stream2 stream2-audio
-    a=rtcp-mux
-    a=rtpmap:120 opus/48000/2
-    a=fmtp:120 minptime=10;useinbandfec=1
-    a=ssrc:120 cname:stream2
-    m=audio 9 UDP/TLS/RTP/SAVPF 120
-    c=IN IP4 0.0.0.0
-    a=rtcp:9 IN IP4 0.0.0.0
-    a=ice-ufrag:1PSY
-    a=ice-pwd:ejBMY08jZ4EWoJbIfuJsgRIS
-    a=ice-options:trickle
-    a=fingerprint:sha-256 24:2D:06:61:0E:59:54:0E:69:08:A4:9F:0A:D9:17:4B:89:50:11:A2:20:65:68:0B:61:11:51:57:EA:F6:11:E4
-    a=setup:actpass
-    a=mid:audio3
-    a=sendonly
-    a=msid:stream3 stream3-audio
-    a=rtcp-mux
-    a=rtpmap:120 opus/48000/2
-    a=fmtp:120 minptime=10;useinbandfec=1
-    a=ssrc:130 cname:stream3
-    m=video 9 UDP/TLS/RTP/SAVPF 96
-    c=IN IP4 0.0.0.0
-    a=rtcp:9 IN IP4 0.0.0.0
-    a=ice-ufrag:1PSY
-    a=ice-pwd:ejBMY08jZ4EWoJbIfuJsgRIS
-    a=ice-options:trickle
-    a=fingerprint:sha-256 24:2D:06:61:0E:59:54:0E:69:08:A4:9F:0A:D9:17:4B:89:50:11:A2:20:65:68:0B:61:11:51:57:EA:F6:11:E4
-    a=setup:actpass
-    a=mid:video1
-    a=sendrecv
-    a=msid:stream1 stream1-video
-    a=rtcp-mux
-    a=rtcp-rsize
-    a=rtpmap:96 H264/90000
-    a=fmtp:96 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f
-    a=ssrc-group:FID 210 211
-    a=ssrc:210 cname:stream1
-    a=ssrc:211 cname:stream1
-    m=video 9 UDP/TLS/RTP/SAVPF 96
-    c=IN IP4 0.0.0.0
-    a=rtcp:9 IN IP4 0.0.0.0
-    a=ice-ufrag:1PSY
-    a=ice-pwd:ejBMY08jZ4EWoJbIfuJsgRIS
-    a=ice-options:trickle
-    a=fingerprint:sha-256 24:2D:06:61:0E:59:54:0E:69:08:A4:9F:0A:D9:17:4B:89:50:11:A2:20:65:68:0B:61:11:51:57:EA:F6:11:E4
-    a=setup:actpass
-    a=mid:video2
-    a=sendonly
-    a=msid:stream2 stream2-video
-    a=rtcp-mux
-    a=rtcp-rsize
-    a=rtpmap:96 H264/90000
-    a=fmtp:96 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f
-    a=ssrc-group:FID 220 221
-    a=ssrc:220 cname:stream2
-    a=ssrc:221 cname:stream2
-    m=video 9 UDP/TLS/RTP/SAVPF 96
-    c=IN IP4 0.0.0.0
-    a=rtcp:9 IN IP4 0.0.0.0
-    a=ice-ufrag:1PSY
-    a=ice-pwd:ejBMY08jZ4EWoJbIfuJsgRIS
-    a=ice-options:trickle
-    a=fingerprint:sha-256 24:2D:06:61:0E:59:54:0E:69:08:A4:9F:0A:D9:17:4B:89:50:11:A2:20:65:68:0B:61:11:51:57:EA:F6:11:E4
-    a=setup:actpass
-    a=mid:video3
-    a=sendonly
-    a=msid:stream3 stream3-video
-    a=rtcp-mux
-    a=rtcp-rsize
-    a=rtpmap:96 H264/90000
-    a=fmtp:96 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f
-    a=ssrc-group:FID 230 231
-    a=ssrc:230 cname:stream3
-    a=ssrc:231 cname:stream3
+    #{sdp_stream(:audio, "recvonly", hd(streams))}
+    #{Enum.map_join(tl(streams), &sdp_stream(:audio, "sendonly", &1))}
+    #{sdp_stream(:video, "recvonly", hd(streams))}
+    #{Enum.map_join(tl(streams), &sdp_stream(:video, "sendonly", &1))}
     """
+    |> String.replace("\n\n", "\n")
+    |> String.replace("\n\n", "\n")
     |> ExSDP.parse()
+  end
+
+  defp sdp_stream(:audio, mode, id) do
+    """
+    m=audio 9 UDP/TLS/RTP/SAVPF 120
+    c=IN IP4 0.0.0.0
+    a=rtcp:9 IN IP4 0.0.0.0
+    a=ice-ufrag:1PSY
+    a=ice-pwd:ejBMY08jZ4EWoJbIfuJsgRIS
+    a=ice-options:trickle
+    a=fingerprint:sha-256 24:2D:06:61:0E:59:54:0E:69:08:A4:9F:0A:D9:17:4B:89:50:11:A2:20:65:68:0B:61:11:51:57:EA:F6:11:E4
+    a=setup:actpass
+    a=mid:audio#{id}
+    a=#{mode}
+    a=msid:stream#{id} stream#{id}-audio
+    a=rtcp-mux
+    a=rtpmap:120 opus/48000/2
+    a=fmtp:120 minptime=10;useinbandfec=1
+    a=ssrc:1#{id}0 cname:stream#{id}
+    """
+  end
+
+  defp sdp_stream(:video, mode, id) do
+    """
+    m=video 9 UDP/TLS/RTP/SAVPF 96
+    c=IN IP4 0.0.0.0
+    a=rtcp:9 IN IP4 0.0.0.0
+    a=ice-ufrag:1PSY
+    a=ice-pwd:ejBMY08jZ4EWoJbIfuJsgRIS
+    a=ice-options:trickle
+    a=fingerprint:sha-256 24:2D:06:61:0E:59:54:0E:69:08:A4:9F:0A:D9:17:4B:89:50:11:A2:20:65:68:0B:61:11:51:57:EA:F6:11:E4
+    a=setup:actpass
+    a=mid:video#{id}
+    a=#{mode}
+    a=msid:stream#{id} stream#{id}-video
+    a=rtcp-mux
+    a=rtcp-rsize
+    a=rtpmap:96 H264/90000
+    a=rtcp-fb:96 ccm fir
+    a=fmtp:96 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f
+    a=ssrc-group:FID 2#{id}0 2#{id}1
+    a=ssrc:2#{id}0 cname:stream#{id}
+    a=ssrc:2#{id}1 cname:stream#{id}
+    """
   end
 end

acfb300a8609539cb1f3f6bdcca4fbbb633a2c8c
Jakub Perzylo <jakub.perzylo@swmansion.com>
2021-03-02 14:01:22 +0100

    Separated configs

--- webrtc/videoroom/config/config.exs
+++ webrtc/videoroom/config/config.exs
@@ -1,44 +1,15 @@
 import Config
 
-config :membrane_videoroom_demo,
-  ip: {0, 0, 0, 0},
-  port: 8443,
-  keyfile: "priv/certs/key.pem",
-  certfile: "priv/certs/certificate.pem"
-
 config :phoenix, :json_library, Jason
 
 config :membrane_videoroom_demo, VideoRoomWeb.Endpoint,
-  url: [host: "localhost"],
   pubsub_server: VideoRoom.PubSub,
   https: [
-    port: 8443,
-    cipher_suite: :strong,
     otp_app: :membrane_videoroom_demo,
-    keyfile: "priv/certs/key.pem",
-    certfile: "priv/certs/certificate.pem"
-  ],
-  watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
-      cd: Path.expand("../assets", __DIR__)
-    ]
-  ],
-  code_reloader: true,
-  live_reload: [
-    dirs: [
-      "priv/static",
-      "lib/videoroom_web/controllers",
-      "lib/videoroom_web/views",
-      "lib/videoroom_web/templates"
-    ]
+    cipher_suite: :strong
   ]
 
 config :logger,
-  level: :debug,
   compile_time_purge_matching: [
     [level_lower_than: :info],
     # Silence irrelevant warnings caused by resending handshake events
@@ -46,3 +17,5 @@ config :logger,
   ]
 
 config :logger, :console, metadata: [:room]
+
+import_config("#{config_env()}.exs")

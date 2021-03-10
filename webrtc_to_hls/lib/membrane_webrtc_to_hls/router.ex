3928ffbef21fbd87ab3af13f71606c1ecd28c6d6
Jakub Perzylo <jakub.perzylo@swmansion.com>
2021-03-10 12:11:01 +0100

    Added routing for HLS files

--- webrtc_to_hls/lib/membrane_webrtc_to_hls/router.ex
+++ webrtc_to_hls/lib/membrane_webrtc_to_hls/router.ex
@@ -9,9 +9,19 @@ defmodule Membrane.Demo.WebRTCToHLS.Router do
   plug(:match)
   plug(:dispatch)
 
+  get "/video/:user/:file" do
+    path = "output/#{user}/#{file}"
+    if File.exists?(path) do
+      send_file(conn, 200, path)
+    else
+      send_resp(conn, 404, "File not found")
+    end
+  end
+
   get "/" do
     send_file(conn, 200, "priv/static/html/index.html")
   end
+
 
   match _ do
     send_resp(conn, 404, "404")

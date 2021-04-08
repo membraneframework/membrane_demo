d6ad6ac3954686762a282c9cdc8d0740888c8419
Michał Śledź <sledzson08@gmail.com>
2021-04-08 16:58:45 +0200

    Use common dtls key/cert

--- webrtc/videoroom/lib/videoroom/application.ex
+++ webrtc/videoroom/lib/videoroom/application.ex
@@ -4,6 +4,8 @@ defmodule VideoRoom.Application do
 
   @impl true
   def start(_type, _args) do
+    config_common_dtls_key_cert()
+
     children = [
       VideoRoomWeb.Endpoint,
       {Phoenix.PubSub, name: VideoRoom.PubSub},
@@ -12,5 +14,14 @@ defmodule VideoRoom.Application do
 
     opts = [strategy: :one_for_one, name: __MODULE__]
     Supervisor.start_link(children, opts)
+  end
+
+  defp config_common_dtls_key_cert() do
+    {:ok, pid} = ExDTLS.start_link(client_mode: false, dtls_srtp: true)
+    {:ok, pkey} = ExDTLS.get_pkey(pid)
+    {:ok, cert} = ExDTLS.get_cert(pid)
+    Process.exit(pid, :normal)
+    Application.put_env(:membrane_videoroom_demo, :dtls_pkey, pkey)
+    Application.put_env(:membrane_videoroom_demo, :dtls_cert, cert)
   end
 end

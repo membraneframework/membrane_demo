1f70da89c62b9c1cbaec2b9fcc1b05242c91392f
agga1 <agnieszka.dutka@swmansion.com>
2021-08-05 16:49:49 +0200

    deps

--- video_mixer/mix.exs
+++ video_mixer/mix.exs
@@ -30,7 +30,8 @@ defmodule VideoMixer.MixProject do
       {:membrane_wav_plugin, "~> 0.1.0"},
       {:membrane_mp4_plugin, "~> 0.6.0"},
       {:membrane_h264_ffmpeg_plugin, "~> 0.10.0"},
-      {:membrane_aac_fdk_plugin, "~> 0.6.0"},
+      {:membrane_aac_fdk_plugin, "~> 0.6.1"},
+      {:membrane_audio_mixer_plugin, "~> 0.6.1"},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end

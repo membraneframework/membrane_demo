4a28081caa3cfa16f468a7067870491db14ad817
Mateusz Front <mateusz.front@swmansion.com>
2021-08-06 11:19:47 +0200

    rtp-to-hls: update deps

--- rtp_to_hls/mix.exs
+++ rtp_to_hls/mix.exs
@@ -22,19 +22,19 @@ defmodule Membrane.Demo.RtpToHls.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:membrane_core, "~> 0.6.0"},
-      {:membrane_element_udp, "~> 0.4.0"},
-      {:membrane_file_plugin, "~> 0.5.0"},
-      {:membrane_h264_ffmpeg_plugin, "~> 0.7.0"},
-      {:membrane_http_adaptive_stream_plugin, "~> 0.2.0"},
-      {:membrane_aac_format, "~> 0.2.0"},
-      {:membrane_mp4_plugin, "~> 0.4.0"},
-      {:membrane_rtp_aac_plugin, "~> 0.3.0-alpha"},
-      {:membrane_rtp_plugin, github: "membraneframework/membrane_rtp_plugin", override: true},
-      {:membrane_rtp_h264_plugin, "~> 0.4.1"},
-      {:membrane_element_tee, "~> 0.4.1"},
-      {:membrane_element_fake, "~> 0.4"},
-      {:membrane_aac_plugin, "~> 0.6.0"},
+      {:membrane_core, "~> 0.7.0"},
+      {:membrane_element_udp, "~> 0.5.0"},
+      {:membrane_file_plugin, "~> 0.6.0"},
+      {:membrane_h264_ffmpeg_plugin, "~> 0.9.0"},
+      {:membrane_http_adaptive_stream_plugin, "~> 0.3.0"},
+      {:membrane_aac_format, "~> 0.3.0"},
+      {:membrane_mp4_plugin, "~> 0.6.0"},
+      {:membrane_rtp_aac_plugin, "~> 0.4.0"},
+      {:membrane_rtp_plugin, "~> 0.6.0"},
+      {:membrane_rtp_h264_plugin, "~> 0.5.0"},
+      {:membrane_element_tee, "~> 0.5.0"},
+      {:membrane_element_fake, "~> 0.5.0"},
+      {:membrane_aac_plugin, "~> 0.7.0"},
       {:ex_libsrtp, "~> 0.1.0"},
       {:membrane_rtp_format, "~> 0.3.0"}
     ]

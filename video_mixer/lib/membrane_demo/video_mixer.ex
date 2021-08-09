2e8bcb5545ea72f40121854606fef75f028b361b
Agnieszka Dutka <agud100@gmail.com>
2021-08-09 11:44:30 +0200

    audio and video mixed in separate pipelines

--- video_mixer/lib/membrane_demo/video_mixer.ex
+++ video_mixer/lib/membrane_demo/video_mixer.ex
@@ -5,36 +5,11 @@ defmodule Membrane.Demo.VideoMixer do
   use Membrane.Pipeline
 
   @impl true
-  def handle_init({path_to_wav_1, path_to_wav_2}) do
-    children = %{
-      # Stream from file
-      file_1: %Membrane.File.Source{location: path_to_wav_1},
-      file_2: %Membrane.File.Source{location: path_to_wav_2},
-      parser_1: Membrane.WAV.Parser,
-      parser_2: Membrane.WAV.Parser,
-      mixer: %Membrane.AudioMixer{
-        caps: %Membrane.Caps.Audio.Raw{
-          channels: 1,
-          sample_rate: 16_000,
-          format: :s16le
-        }
-      },
-      aac_fdk: Membrane.AAC.FDK.Encoder,
-      file_sink: %Membrane.File.Sink{location: "output.aac"}
-    }
+  def handle_init(opts) do
+    children = %{}
 
     # Setup the flow of the data
-    links = [
-      link(:file_1)
-      |> to(:parser_1)
-      |> to(:mixer)
-      |> to(:aac_fdk)
-      |> to(:file_sink),
-      link(:file_2)
-      |> to(:parser_2)
-      |> via_in(:input, options: [offset: Membrane.Time.milliseconds(2000)])
-      |> to(:mixer)
-    ]
+    links = []
 
     {{:ok, spec: %ParentSpec{children: children, links: links}}, %{}}
   end
--- video_mixer/run.exs
+++ video_mixer/run.exs
@@ -3,5 +3,5 @@
 # VideoMixer.play(pid)
 
 alias Membrane.Demo.VideoOnly
-{:ok, pid} = VideoOnly.start_link({"test_video.h264", "test_video.h264"})
+{:ok, pid} = VideoOnly.start_link({"test_video2.h264", "test_video2.h264"})
 VideoOnly.play(pid)

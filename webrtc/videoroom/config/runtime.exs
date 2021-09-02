4abb0185df2ccb58e650e03f747a52277277093e
Roberto Ostinelli <roberto@ostinelli.net>
2021-09-02 17:42:30 +0200

    Allow to specify http port with the environment variable SERVER_PORT.

--- webrtc/videoroom/config/runtime.exs
+++ webrtc/videoroom/config/runtime.exs
@@ -2,5 +2,5 @@ import Config
 
 config :membrane_videoroom_demo, VideoRoomWeb.Endpoint, [
   {:url, [host: "localhost"]},
-  {:http, [otp_app: :membrane_videoroom_demo, port: 4000]}
+  {:http, [otp_app: :membrane_videoroom_demo, port: System.get_env("SERVER_PORT") || 4000]}
 ]

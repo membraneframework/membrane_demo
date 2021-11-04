cec4b77e3637363d439384bd3fb13cdedc975eb3
marcinwasowicz <marcinkwasowicz@gmail.com>
2021-11-04 13:04:35 +0100

    Format and adjust pipeline to simulcast version of WebRTC

--- webrtc_to_hls/lib/webrtc_to_hls/pipeline.ex
+++ webrtc_to_hls/lib/webrtc_to_hls/pipeline.ex
@@ -1,10 +1,11 @@
 defmodule WebRTCToHLS.Pipeline do
   use Membrane.Pipeline
-
-  require Membrane.Logger
+  import Membrane.RTC.Utils
 
   alias Membrane.WebRTC.{Endpoint, EndpointBin, Track}
   alias Membrane.RTC.Engine.MediaEvent
+
+  require Membrane.Logger
 
   import WebRTCToHLS.Helpers
 
@@ -17,6 +18,10 @@ defmodule WebRTCToHLS.Pipeline do
           vad: boolean()
         ]
 
+  @type packet_filters_t() :: %{
+          (encoding_name :: atom()) => [Membrane.RTP.SessionBin.packet_filter_t()]
+        }
+
   @type network_options_t() :: [
           stun_servers: [stun_server_t()],
           turn_servers: [turn_server_t()],
@@ -27,7 +32,10 @@ defmodule WebRTCToHLS.Pipeline do
   @type options_t() :: [
           id: String.t(),
           extension_options: extension_options_t(),
-          network_options: network_options_t()
+          network_options: network_options_t(),
+          packet_filters: %{
+            (encoding_name :: atom()) => [packet_filters_t()]
+          }
         ]
 
   @spec start(options :: options_t(), process_options :: GenServer.options()) ::
@@ -55,6 +63,9 @@ defmodule WebRTCToHLS.Pipeline do
     ])
   end
 
+  @spec get_registry_name() :: atom()
+  def get_registry_name(), do: @registry_name
+
   @impl true
   def handle_init(options) do
     play(self())
@@ -63,13 +74,12 @@ defmodule WebRTCToHLS.Pipeline do
      %{
        id: options[:id],
        peers: %{},
-       incoming_peers: %{},
        endpoints: %{},
-       options: options
+       options: options,
+       packet_filters: options[:packet_filters] || %{},
+       waiting_for_linking: %{}
      }}
   end
-
-  defp get_registry_name(), do: @registry_name
 
   @impl true
   def handle_other({:register, pid}, _ctx, state) do
@@ -94,8 +104,13 @@ defmodule WebRTCToHLS.Pipeline do
   def handle_other({:media_event, from, data}, ctx, state) do
     case MediaEvent.deserialize(data) do
       {:ok, event} ->
-        {actions, state} = handle_media_event(event, from, ctx, state)
-        {{:ok, actions}, state}
+        if event.type == :join or Map.has_key?(state.peers, from) do
+          {actions, state} = handle_media_event(event, from, ctx, state)
+          {{:ok, actions}, state}
+        else
+          Membrane.Logger.warn("Received media event from unknown peer id: #{inspect(from)}")
+          {:ok, state}
+        end
 
       {:error, :invalid_media_event} ->
         Membrane.Logger.warn("Invalid media event #{inspect(data)}")
@@ -104,54 +119,50 @@ defmodule WebRTCToHLS.Pipeline do
   end
 
   defp handle_media_event(%{type: :join, data: data}, peer_id, ctx, state) do
-    dispatch({:new_peer, peer_id, data.metadata, data.tracks_metadata}, state)
+    dispatch({:new_peer, peer_id, data.metadata})
 
     receive do
       {:accept_new_peer, ^peer_id} ->
-        peer = Map.put(data, :id, peer_id)
-        state = put_in(state, [:incoming_peers, peer_id], peer)
-        {actions, state} = setup_peer(peer, ctx, state)
+        do_accept_new_peer(peer_id, node(), data, ctx, state)
 
-        MediaEvent.create_peer_accepted_event(peer_id, Map.delete(state.peers, peer_id))
-        |> dispatch(state)
+      {:accept_new_peer, ^peer_id, peer_node} ->
+        do_accept_new_peer(peer_id, peer_node, data, ctx, state)
 
-        {actions, state}
-
-      {:accept_new_peer, _other_peer_id} ->
+      {:accept_new_peer, peer_id} ->
         Membrane.Logger.warn("Unknown peer id passed for acceptance: #{inspect(peer_id)}")
+        {[], state}
+
+      {:accept_new_peer, peer_id, peer_node} ->
+        Membrane.Logger.warn(
+          "Unknown peer id passed for acceptance: #{inspect(peer_id)} for node #{inspect(peer_node)}"
+        )
+
         {[], state}
 
       {:deny_new_peer, peer_id} ->
         MediaEvent.create_peer_denied_event(peer_id)
-        |> dispatch(state)
+        |> dispatch()
+
+        {[], state}
+
+      {:deny_new_peer, peer_id, data: data} ->
+        MediaEvent.create_peer_denied_event(peer_id, data)
+        |> dispatch()
 
         {[], state}
     end
   end
 
-  defp handle_media_event(%{type: :sdp_answer} = event, peer_id, _ctx, state) do
+  defp handle_media_event(%{type: :sdp_offer} = event, peer_id, _ctx, state) do
     actions = [
-      forward: {{:endpoint, peer_id}, {:signal, {:sdp_answer, event.data.sdp_answer.sdp}}}
+      forward:
+        {{:endpoint, peer_id},
+         {:signal, {:sdp_offer, event.data.sdp_offer.sdp, event.data.mid_to_track_id}}}
     ]
 
-    state =
-      if Map.has_key?(state.incoming_peers, peer_id) do
-        {peer, state} = pop_in(state, [:incoming_peers, peer_id])
-        peer = Map.delete(peer, :tracks_metadata)
-        peer = Map.put(peer, :mid_to_track_metadata, event.data.mid_to_track_metadata)
-        state = put_in(state, [:peers, peer_id], peer)
-
-        MediaEvent.create_peer_joined_event(
-          peer_id,
-          state.peers[peer_id].metadata,
-          event.data.mid_to_track_metadata
-        )
-        |> dispatch(state)
-
-        state
-      else
-        state
-      end
+    peer = get_in(state, [:peers, peer_id])
+    peer = Map.put(peer, :track_id_to_track_metadata, event.data.track_id_to_track_metadata)
+    state = put_in(state, [:peers, peer_id], peer)
 
     {actions, state}
   end
@@ -167,10 +178,73 @@ defmodule WebRTCToHLS.Pipeline do
     {[], state}
   end
 
+  defp handle_media_event(%{type: :renegotiate_tracks}, peer_id, _ctx, state) do
+    actions = [forward: {{:endpoint, peer_id}, {:signal, :renegotiate_tracks}}]
+    {actions, state}
+  end
+
+  defp handle_media_event(
+         %{type: :update_peer_metadata, data: %{metadata: metadata}},
+         peer_id,
+         _ctx,
+         state
+       ) do
+    peer = Map.get(state.peers, peer_id)
+
+    if peer.metadata != metadata do
+      updated_peer = %{peer | metadata: metadata}
+      state = put_in(state, [:peers, peer_id], updated_peer)
+
+      MediaEvent.create_peer_updated_event(peer_id, updated_peer)
+      |> dispatch()
+
+      {[], state}
+    else
+      {[], state}
+    end
+  end
+
+  defp handle_media_event(
+         %{
+           type: :update_track_metadata,
+           data: %{track_id: track_id, track_metadata: track_metadata}
+         },
+         peer_id,
+         _ctx,
+         state
+       ) do
+    peer = Map.get(state.peers, peer_id)
+
+    if Map.get(peer.track_id_to_track_metadata, track_id) != track_metadata do
+      peer = Map.update!(peer, :track_id_to_track_metadata, &%{&1 | track_id => track_metadata})
+      state = put_in(state, [:peers, peer_id], peer)
+
+      MediaEvent.create_track_updated_event(peer_id, track_id, track_metadata)
+      |> dispatch()
+
+      {[], state}
+    else
+      {[], state}
+    end
+  end
+
+  @impl true
+  def handle_notification(
+        {:signal, {:sdp_answer, answer, mid_to_track_id}},
+        {:endpoint, peer_id},
+        _ctx,
+        state
+      ) do
+    MediaEvent.create_signal_event(peer_id, {:signal, {:sdp_answer, answer, mid_to_track_id}})
+    |> dispatch()
+
+    {:ok, state}
+  end
+
   @impl true
   def handle_notification({:signal, message}, {:endpoint, peer_id}, _ctx, state) do
     MediaEvent.create_signal_event(peer_id, {:signal, message})
-    |> dispatch(state)
+    |> dispatch()
 
     {:ok, state}
   end
@@ -208,7 +282,7 @@ defmodule WebRTCToHLS.Pipeline do
   end
 
   def handle_notification({:vad, val}, {:endpoint, endpoint_id}, _ctx, state) do
-    dispatch({:vad_notification, val, endpoint_id}, state)
+    dispatch({:vad_notification, val, endpoint_id})
     {:ok, state}
   end
 
@@ -237,8 +311,136 @@ defmodule WebRTCToHLS.Pipeline do
     end)
   end
 
-  defp setup_peer(config, _ctx, state) do
-    inbound_tracks = create_inbound_tracks(config.relay_audio, config.relay_video)
+  @impl true
+  def handle_notification(
+        {:negotiation_done, new_outbound_tracks},
+        {:endpoint, endpoint_id},
+        ctx,
+        state
+      ) do
+    {new_links, new_waiting_for_linking} =
+      link_outbound_tracks(new_outbound_tracks, endpoint_id, ctx)
+
+    state =
+      update_in(
+        state,
+        [:waiting_for_linking, endpoint_id],
+        &MapSet.union(&1, new_waiting_for_linking)
+      )
+
+    {{:ok, [spec: %ParentSpec{links: new_links}]}, state}
+  end
+
+  @impl true
+  def handle_notification({:new_tracks, tracks}, {:endpoint, endpoint_id}, ctx, state) do
+    id_to_track = Map.new(tracks, &{&1.id, &1})
+
+    state =
+      update_in(state, [:endpoints, endpoint_id, :inbound_tracks], &Map.merge(&1, id_to_track))
+
+    tracks_msgs = update_track_messages(ctx, {:add_tracks, tracks}, {:endpoint, endpoint_id})
+
+    peer = get_in(state, [:peers, endpoint_id])
+
+    MediaEvent.create_tracks_added_event(endpoint_id, peer.track_id_to_track_metadata)
+    |> dispatch()
+
+    {{:ok, tracks_msgs}, state}
+  end
+
+  @impl true
+  def handle_notification({:removed_tracks, tracks}, {:endpoint, endpoint_id}, ctx, state) do
+    id_to_track = Map.new(tracks, &{&1.id, &1})
+
+    state =
+      update_in(state, [:endpoints, endpoint_id, :inbound_tracks], &Map.merge(&1, id_to_track))
+
+    tracks_msgs = update_track_messages(ctx, {:remove_tracks, tracks}, {:endpoint, endpoint_id})
+    track_ids = Enum.map(tracks, & &1.id)
+
+    MediaEvent.create_tracks_removed_event(endpoint_id, track_ids)
+    |> dispatch()
+
+    {{:ok, tracks_msgs}, state}
+  end
+
+  defp link_inbound_track({track_id, encoding}, tee, waiting_for_linking, ctx) do
+    reduce_children(ctx, {[], waiting_for_linking}, fn
+      {:endpoint, endpoint_id}, {new_links, waiting_for_linking} ->
+        if MapSet.member?(waiting_for_linking[endpoint_id], track_id) do
+          new_link =
+            link(tee)
+            |> via_out(:copy)
+            |> via_in(Pad.ref(:input, track_id), options: [encoding: encoding])
+            |> to({:endpoint, endpoint_id})
+
+          waiting_for_linking =
+            Map.update!(waiting_for_linking, endpoint_id, &MapSet.delete(&1, track_id))
+
+          {new_links ++ [new_link], waiting_for_linking}
+        else
+          {new_links, waiting_for_linking}
+        end
+
+      _other_child, {new_links, waiting_for_linking} ->
+        {new_links, waiting_for_linking}
+    end)
+  end
+
+  defp link_outbound_tracks(tracks, endpoint_id, ctx) do
+    Enum.reduce(tracks, {[], MapSet.new()}, fn
+      {track_id, encoding}, {new_links, not_linked} ->
+        tee = find_child(ctx, pattern: {:tee, {_other_endpoint_id, ^track_id}})
+
+        if tee do
+          new_link =
+            link(tee)
+            |> via_out(:copy)
+            |> via_in(Pad.ref(:input, track_id), options: [encoding: encoding])
+            |> to({:endpoint, endpoint_id})
+
+          {new_links ++ [new_link], not_linked}
+        else
+          {new_links, MapSet.put(not_linked, track_id)}
+        end
+
+      _track, {new_links, not_linked} ->
+        {new_links, not_linked}
+    end)
+  end
+
+  defp dispatch(msg) do
+    Registry.dispatch(get_registry_name(), self(), fn entries ->
+      for {_, pid} <- entries, do: send(pid, {self(), msg})
+    end)
+  end
+
+  defp do_accept_new_peer(peer_id, peer_node, data, ctx, state) do
+    if Map.has_key?(state.peers, peer_id) do
+      Membrane.Logger.warn("Peer with id: #{inspect(peer_id)} has already been added")
+      {[], state}
+    else
+      peer =
+        if Map.has_key?(data, :track_id_to_track_metadata),
+          do: data,
+          else: Map.put(data, :track_id_to_track_metadata, %{})
+
+      peer = Map.put(peer, :id, peer_id)
+
+      state = put_in(state, [:peers, peer_id], peer)
+      {actions, state} = setup_peer(peer, peer_node, ctx, state)
+
+      MediaEvent.create_peer_accepted_event(peer_id, Map.delete(state.peers, peer_id))
+      |> dispatch()
+
+      MediaEvent.create_peer_joined_event(peer_id, peer) |> dispatch()
+
+      {actions, state}
+    end
+  end
+
+  defp setup_peer(config, peer_node, _ctx, state) do
+    inbound_tracks = []
     outbound_tracks = get_outbound_tracks(state.endpoints, config.receive_media)
 
     # FIXME `type` field should probably be deleted from Endpoint struct
@@ -253,8 +455,8 @@ defmodule WebRTCToHLS.Pipeline do
         [
           client_mode: false,
           dtls_srtp: true,
-          pkey: state.options.network_options.dtls_pkey,
-          cert: state.options.network_options.dtls_cert
+          pkey: state.options[:network_options][:dtls_pkey],
+          cert: state.options[:network_options][:dtls_cert]
         ]
       else
         [
@@ -278,16 +480,14 @@ defmodule WebRTCToHLS.Pipeline do
         inbound_tracks: inbound_tracks,
         stun_servers: state.options[:network_options][:stun_servers] || [],
         turn_servers: state.options[:network_options][:turn_servers] || [],
-        video_codecs: [
-          %ExSDP.Attribute.RTPMapping{payload_type: 96, encoding: "H264", clock_rate: 90_000},
-          %ExSDP.Attribute.FMTP{
-            pt: 96,
-            level_asymmetry_allowed: true,
-            packetization_mode: 1,
-            profile_level_id: 0x42E01F
-          }
-        ],
-        use_default_codecs: [:audio],
+        filter_codecs: fn {rtp, fmtp} ->
+          case rtp.encoding do
+            "opus" -> true
+            "VP8" -> false
+            "H264" -> fmtp.profile_level_id === 0x42E01F
+            _unsupported_codec -> false
+          end
+        end,
         handshake_opts: handshake_opts,
         log_metadata: [peer_id: config.id]
       },
@@ -300,7 +500,13 @@ defmodule WebRTCToHLS.Pipeline do
       }
     }
 
-    spec = %ParentSpec{children: children, links: [], crash_group: {config.id, :temporary}}
+    state = put_in(state, [:waiting_for_linking, config.id], MapSet.new())
+
+    spec = %ParentSpec{
+      node: peer_node,
+      children: children,
+      crash_group: {config.id, :temporary}
+    }
 
     state = put_in(state.endpoints[config.id], endpoint)
 
@@ -370,5 +576,64 @@ defmodule WebRTCToHLS.Pipeline do
 
   defp setup_extensions(encoding, extension_options) do
     if encoding == :OPUS and extension_options[:vad], do: [{:vad, Membrane.RTP.VAD}], else: []
+  end
+
+  defp remove_peer(peer_id, ctx, state) do
+    case do_remove_peer(peer_id, ctx, state) do
+      {:absent, [], state} ->
+        Membrane.Logger.info("Peer #{inspect(peer_id)} already removed")
+        {[], state}
+
+      {:present, actions, state} ->
+        {_waiting, state} = pop_in(state, [:waiting_for_linking, peer_id])
+
+        MediaEvent.create_peer_left_event(peer_id)
+        |> dispatch()
+
+        {actions, state}
+    end
+  end
+
+  defp do_remove_peer(peer_id, ctx, state) do
+    if Map.has_key?(state.endpoints, peer_id) do
+      {endpoint, state} = pop_in(state, [:endpoints, peer_id])
+      {_peer, state} = pop_in(state, [:peers, peer_id])
+      tracks = Enum.map(Endpoint.get_tracks(endpoint), &%Track{&1 | status: :disabled})
+
+      tracks_msgs = update_track_messages(ctx, {:remove_tracks, tracks}, {:endpoint, peer_id})
+
+      endpoint_bin = ctx.children[{:endpoint, peer_id}]
+
+      actions =
+        if endpoint_bin == nil or endpoint_bin.terminating? do
+          []
+        else
+          children =
+            Endpoint.get_tracks(endpoint)
+            |> Enum.map(fn track -> track.id end)
+            |> Enum.flat_map(&[tee: {peer_id, &1}, fake: {peer_id, &1}])
+            |> Enum.filter(&Map.has_key?(ctx.children, &1))
+
+          children = [endpoint: peer_id] ++ children
+          [remove_child: children]
+        end
+
+      {:present, tracks_msgs ++ actions, state}
+    else
+      {:absent, [], state}
+    end
+  end
+
+  defp update_track_messages(_ctx, [] = _tracks, _endpoint_bin), do: []
+
+  defp update_track_messages(ctx, msg, endpoint_bin_name) do
+    flat_map_children(ctx, fn
+      {:endpoint, _endpoint_id} = other_endpoint_bin
+      when other_endpoint_bin != endpoint_bin_name ->
+        [forward: {other_endpoint_bin, msg}]
+
+      _child ->
+        []
+    end)
   end
 end

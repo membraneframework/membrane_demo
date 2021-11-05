0ee2665a97155e558bdcdda3ecf009145847d087
Jakub Perżyło <jakub.perzylo@swmansion.com>
2021-11-05 12:59:56 +0100

    Updated videoroom advanced to reflex videoroom UI changes (#153)
    * Updated videoroom advanced to reflex videoroom UI changes
    
    * Updated Dockerfile
    
    * Updated private assets

--- webrtc/videoroom_advanced/lib/videoroom/room.ex
+++ webrtc/videoroom_advanced/lib/videoroom/room.ex
@@ -19,54 +19,65 @@ defmodule Videoroom.Room do
 
   @impl true
   def init(opts) do
-    Membrane.Logger.info("Spawning room proces: #{inspect(self())}")
+    Membrane.Logger.info("Spawning room process: #{inspect(self())}")
 
-    rtc_engine_options = [
+    sfu_options = [
       id: opts[:room_id],
       network_options: [
-        stun_servers: Application.fetch_env!(:membrane_videoroom_demo, :stun_servers),
-        turn_servers: Application.fetch_env!(:membrane_videoroom_demo, :turn_servers)
-      ]
+        stun_servers: [
+          %{server_addr: "stun.l.google.com", server_port: 19_302}
+        ],
+        turn_servers: [],
+        dtls_pkey: Application.get_env(:membrane_videoroom_demo, :dtls_pkey),
+        dtls_cert: Application.get_env(:membrane_videoroom_demo, :dtls_cert)
+      ],
+      payload_and_depayload_tracks?: false
     ]
 
-    {:ok, pid} = Membrane.RTC.Engine.start(rtc_engine_options, [])
+    {:ok, pid} = Membrane.RTC.Engine.start(sfu_options, [])
     send(pid, {:register, self()})
-    {:ok, %{rtc_engine: pid, peer_channels: %{}}}
+    {:ok, %{sfu_engine: pid, peer_channels: %{}}}
   end
 
   @impl true
-  def handle_call({:add_peer_channel, peer_channel_pid, peer_pid}, _from, state) do
-    state = put_in(state, [:peer_channels, peer_pid], peer_channel_pid)
+  def handle_call({:add_peer_channel, peer_channel_pid, peer_id}, _from, state) do
+    state = put_in(state, [:peer_channels, peer_id], peer_channel_pid)
     Process.monitor(peer_channel_pid)
     {:reply, :ok, state}
   end
 
   @impl true
-  def handle_info({_rtc_engine, {:sfu_media_event, :broadcast, event}}, state) do
+  def handle_info({_sfu_engine, {:sfu_media_event, :broadcast, event}}, state) do
     for {_peer_id, pid} <- state.peer_channels, do: send(pid, {:media_event, event})
     {:noreply, state}
   end
 
   @impl true
-  def handle_info({_rtc_engine, {:sfu_media_event, to, event}}, state) do
-    send(state.peer_channels[to], {:media_event, event})
+  def handle_info({_sfu_engine, {:sfu_media_event, to, event}}, state) do
+    if state.peer_channels[to] != nil do
+      send(state.peer_channels[to], {:media_event, event})
+    end
+
     {:noreply, state}
   end
 
   @impl true
-  def handle_info({rtc_engine, {:new_peer, peer_id, _metadata}}, state) do
-    send(rtc_engine, {:accept_new_peer, peer_id})
+  def handle_info({sfu_engine, {:new_peer, peer_id, _metadata}}, state) do
+    # get node the peer with peer_id is running on
+    peer_channel_pid = Map.get(state.peer_channels, peer_id)
+    peer_node = node(peer_channel_pid)
+    send(sfu_engine, {:accept_new_peer, peer_id, peer_node})
     {:noreply, state}
   end
 
   @impl true
-  def handle_info({_rtc_engine, {:peer_left, _peer_id}}, state) do
+  def handle_info({_sfu_engine, {:peer_left, _peer_id}}, state) do
     {:noreply, state}
   end
 
   @impl true
   def handle_info({:media_event, _from, _event} = msg, state) do
-    send(state.rtc_engine, msg)
+    send(state.sfu_engine, msg)
     {:noreply, state}
   end
 
@@ -76,7 +87,7 @@ defmodule Videoroom.Room do
       state.peer_channels
       |> Enum.find(fn {_peer_id, peer_channel_pid} -> peer_channel_pid == pid end)
 
-    send(state.rtc_engine, {:remove_peer, peer_id})
+    send(state.sfu_engine, {:remove_peer, peer_id})
     {_elem, state} = pop_in(state, [:peer_channels, peer_id])
     {:noreply, state}
   end

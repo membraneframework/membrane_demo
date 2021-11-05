70447df13573a64cfeb3a00825567a2b8be7306c
Jakub Perżyło <qizot1405@gmail.com>
2021-11-05 13:18:25 +0100

    Fixed virtual host to use runtime variable

--- webrtc/videoroom_advanced/config/runtime.exs
+++ webrtc/videoroom_advanced/config/runtime.exs
@@ -1,6 +1,6 @@
 import Config
 
 config :membrane_videoroom_demo, VideoRoomWeb.Endpoint, [
-  {:url, [host: "localhost"]},
+  {:url, [host: System.get_env("VIRTUAL_HOST", "localhost")]},
   {:http, [otp_app: :membrane_videoroom_demo, port: System.get_env("SERVER_PORT") || 4000]}
 ]

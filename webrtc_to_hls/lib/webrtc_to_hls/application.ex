396277d0192d02e4c025b47c4c18754d944f4b19
Radosław Szuma <56085570+Rados13@users.noreply.github.com>
2022-01-04 12:21:49 +0100

    Added naive simulcast (#171)
    * Added screen sharing
    
    * Adjust to changes in RTCEngine
    
    * Adjust to new RTC engine
    
    * Use local version of HLS plugin
    
    * Use local http plugin
    
    * Bump erlang and elixir versions
    
    * Custom dependencies
    
    * Enable distributed erlang
    
    * Adjust handle info arity
    
    * Adjust node dependencies
    
    * Adjust node dependencies
    
    * Use HLS from github branch
    
    * Format and adjust pipeline to simulcast version of WebRTC
    
    * Format and adjust pipeline to simulcast version of WebRTC
    
    * Adjust pipeline and js app for multicast
    
    * Add content typa to pad name
    
    * Revert rtp to hls demo to master content
    
    * Remove unused functions
    
    * Adjust dependencies to new HLS implementation
    
    * Use http plugin master
    
    * Removed unused media event handlers
    
    * Use HLS branch with pad issue fix
    
    * Erase commented pipeline children
    
    * Use fixed HLS plugin master
    
    * Use new modular_rtc_engine for webrtc_to_hls
    
    * Update deps
    
    * Adjust StorageCleanup to HLSEndpoint
    
    * Update deps
    
    * Handle cleanup message
    
    * CR changes
    
    * Bump deps
    
    Co-authored-by: Marcin Wąsowicz <marcinwasowicz@Marcins-MacBook-Pro.local>
    Co-authored-by: marcinwasowicz <marcinkwasowicz@gmail.com>

--- webrtc_to_hls/lib/webrtc_to_hls/application.ex
+++ webrtc_to_hls/lib/webrtc_to_hls/application.ex
@@ -2,15 +2,17 @@ defmodule WebRTCToHLS.Application do
   @moduledoc false
   use Application
 
+  alias WebRTCToHLS.StorageCleanup
+
   @impl true
   def start(_type, _args) do
     children = [
       WebRTCToHLSWeb.Endpoint,
       {Phoenix.PubSub, name: WebRTCToHLS.PubSub},
-      {Registry, keys: :duplicate, name: WebRTCToHLS.Registry},
-      WebRTCToHLS.StorageCleanup
+      {Registry, keys: :duplicate, name: WebRTCToHLS.Registry}
     ]
 
+    StorageCleanup.clean_unused_directories()
     opts = [strategy: :one_for_one, name: __MODULE__]
     Supervisor.start_link(children, opts)
   end

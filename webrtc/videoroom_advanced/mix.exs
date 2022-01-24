825a70d504515db535b6654fb58f76939184bd42
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2022-01-24 15:01:21 +0100

    Update branches of dependencies

--- webrtc/videoroom_advanced/mix.exs
+++ webrtc/videoroom_advanced/mix.exs
@@ -22,8 +22,10 @@ defmodule VideoRoom.MixProject do
   defp deps do
     [
       {:membrane_rtc_engine, github: "membraneframework/membrane_rtc_engine"},
-      {:membrane_webrtc_plugin, github: "membraneframework/membrane_webrtc_plugin"},
-      {:membrane_ice_plugin, github: "membraneframework/membrane_turn_plugin"},
+      {:membrane_webrtc_plugin,
+       github: "membraneframework/membrane_webrtc_plugin",
+       branch: "candidate-port-registry",
+       override: true},
       {:plug_cowboy, "~> 2.0"},
       {:phoenix, "~> 1.6"},
       {:phoenix_html, "~> 3.0"},

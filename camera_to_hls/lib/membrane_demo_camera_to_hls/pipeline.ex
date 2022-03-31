a143bbcd93cdc1065a2d92008a18bd3e9f8e7b4b
Dima Mikielewicz <dymitrmikielewicz@gmail.com>
2022-03-31 13:42:45 +0200

    Fixed broken pads

--- camera_to_hls/lib/membrane_demo_camera_to_hls/pipeline.ex
+++ camera_to_hls/lib/membrane_demo_camera_to_hls/pipeline.ex
@@ -7,8 +7,13 @@ defmodule Membrane.Demo.CameraToHls.Pipeline do
       source: Membrane.CameraCapture,
       converter: %Membrane.FFmpeg.SWScale.PixelFormatConverter{format: :I420},
       encoder: Membrane.H264.FFmpeg.Encoder,
+      video_nal_parser: %Membrane.H264.FFmpeg.Parser{
+        framerate: {30, 1},
+        alignment: :au,
+        attach_nalus?: true
+      },
       video_payloader: Membrane.MP4.Payloader.H264,
-      video_cmaf_muxer: Membrane.MP4.CMAF.Muxer,
+      video_cmaf_muxer: Membrane.MP4.Muxer.CMAF,
       hls: %Membrane.HTTPAdaptiveStream.Sink{
         manifest_module: Membrane.HTTPAdaptiveStream.HLS,
         storage: %Membrane.HTTPAdaptiveStream.Storages.FileStorage{directory: "output"}
@@ -19,9 +24,9 @@ defmodule Membrane.Demo.CameraToHls.Pipeline do
       link(:source)
       |> to(:converter)
       |> to(:encoder)
+      |> to(:video_nal_parser)
       |> to(:video_payloader)
       |> to(:video_cmaf_muxer)
-      |> via_in(:input)
       |> to(:hls)
     ]
 
--- camera_to_hls/mix.exs
+++ camera_to_hls/mix.exs
@@ -26,11 +26,11 @@ defmodule Membrane.Demo.CameraToHls.MixProject do
       {:membrane_camera_capture_plugin,
        github: "membraneframework/membrane_camera_capture_plugin"},
       {:membrane_h264_ffmpeg_plugin, "~> 0.18.0"},
-      {:membrane_file_plugin, "~> 0.9.0"},
+      {:membrane_file_plugin, "~> 0.8.0"},
       {:membrane_ffmpeg_swscale_plugin,
        github: "membraneframework/membrane_ffmpeg_swscale_plugin", branch: "pix_fmt_converter"},
-       {:membrane_mp4_plugin, "~> 0.6.0"},
-      {:membrane_http_adaptive_stream_plugin, "~> 0.3.0"}
+      {:membrane_mp4_plugin, "~> 0.11.0"},
+      {:membrane_http_adaptive_stream_plugin, "~> 0.5.0"}
     ]
-  end
+end
 end

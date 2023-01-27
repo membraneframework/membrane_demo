64957eaa50c3021ff4d78d088ad7f03ede80be5b
Jakub Pisarek <99591440+sgfn@users.noreply.github.com>
2023-01-27 11:35:57 +0100

    rtsp debug

--- rtsp_to_hls/lib/pipeline.ex
+++ rtsp_to_hls/lib/pipeline.ex
@@ -9,7 +9,7 @@ defmodule Membrane.Demo.RtspToHls.Pipeline do
   alias Membrane.Demo.RtspToHls.ConnectionManager
 
   @impl true
-  def handle_init(options) do
+  def handle_init(_ctx, options) do
     Logger.debug("Source handle_init options: #{inspect(options)}")
 
     connection_manager_spec = [
@@ -33,71 +33,119 @@ defmodule Membrane.Demo.RtspToHls.Pipeline do
       name: Membrane.Demo.RtspToHls.Supervisor
     )
 
-    {:ok, %{video: nil, port: options.port, output_path: options.output_path}}
+    {[playback: :playing], %{video: nil, port: options.port, output_path: options.output_path}}
   end
 
   @impl true
-  def handle_other({:rtsp_setup_complete, options}, _ctx, state) do
+  def handle_info({:rtsp_setup_complete, options}, _ctx, state) do
     Logger.debug("Source received pipeline options: #{inspect(options)}")
 
-    children = %{
-      app_source: %Membrane.UDP.Source{
+    structure = [
+      child(:app_source, %Membrane.UDP.Source{
         local_port_no: state[:port],
         recv_buffer_size: 500_000
-      },
-      rtp: %Membrane.RTP.SessionBin{
+      }),
+      child(:rtp, %Membrane.RTP.SessionBin{
         fmt_mapping: %{96 => {:H264, 90_000}}
-      },
-      hls: %Membrane.HTTPAdaptiveStream.Sink{
+      }),
+      child(:hls, %Membrane.HTTPAdaptiveStream.SinkBin{
         manifest_module: Membrane.HTTPAdaptiveStream.HLS,
         target_window_duration: 120 |> Membrane.Time.seconds(),
-        target_segment_duration: 4 |> Membrane.Time.seconds(),
+#        target_segment_duration: 4 |> Membrane.Time.seconds(),
         storage: %Membrane.HTTPAdaptiveStream.Storages.FileStorage{
           directory: state[:output_path]
         }
-      }
-    }
-
-    links = [
-      link(:app_source)
+      }),
+      get_child(:app_source)
       |> via_in(Pad.ref(:rtp_input, make_ref()))
-      |> to(:rtp)
+      |> get_child(:rtp)
     ]
 
-    spec = %ParentSpec{children: children, links: links}
-    {{:ok, spec: spec}, %{state | video: %{sps: options[:sps], pps: options[:pps]}}}
+#    children = %{
+#      app_source: %Membrane.UDP.Source{
+#        local_port_no: state[:port],
+#        recv_buffer_size: 500_000
+#      },
+#      rtp: %Membrane.RTP.SessionBin{
+#        fmt_mapping: %{96 => {:H264, 90_000}}
+#      },
+#      hls: %Membrane.HTTPAdaptiveStream.SinkBin{
+#        manifest_module: Membrane.HTTPAdaptiveStream.HLS,
+#        target_window_duration: 120 |> Membrane.Time.seconds(),
+##        target_segment_duration: 4 |> Membrane.Time.seconds(),
+#        storage: %Membrane.HTTPAdaptiveStream.Storages.FileStorage{
+#          directory: state[:output_path]
+#        }
+#      }
+#    }
+
+#    links = [
+#      link(:app_source)
+#      |> via_in(Pad.ref(:rtp_input, make_ref()))
+#      |> to(:rtp)
+#    ]
+
+#    spec = %ParentSpec{children: children, links: links}
+    {[spec: structure], %{state | video: %{sps: options[:sps], pps: options[:pps]}}}
   end
 
   @impl true
-  def handle_notification({:new_rtp_stream, ssrc, 96, _extensions}, :rtp, _ctx, state) do
-    Logger.debug(":new_rtp_stream")
+  def handle_child_notification({:new_rtp_stream, ssrc, 96, _extensions} = msg, :rtp, _ctx, state) do
+    Logger.info("#{inspect msg}")
 
-    children = %{
-      video_nal_parser: %Membrane.H264.FFmpeg.Parser{
+    structure = [
+      child(:video_nal_parser, %Membrane.H264.FFmpeg.Parser{
         sps: state.video.sps,
         pps: state.video.pps,
         skip_until_keyframe?: true,
+        skip_until_parameters?: false,
         framerate: {30, 1},
         alignment: :au,
-        attach_nalus?: true
-      },
-      video_payloader: Membrane.MP4.Payloader.H264,
-      video_cmaf_muxer: Membrane.MP4.Muxer.CMAF
-    }
-
-    links = [
-      link(:rtp)
+        attach_nalus?: true,
+      }),
+      child(:video_payloader, Membrane.MP4.Payloader.H264),
+      child(:video_cmaf_muxer, Membrane.MP4.Muxer.CMAF),
+      get_child(:rtp)
       |> via_out(Pad.ref(:output, ssrc),
         options: [depayloader: Membrane.RTP.H264.Depayloader]
       )
-      |> to(:video_nal_parser)
-      |> to(:video_payloader)
-      |> to(:video_cmaf_muxer)
-      |> via_in(:input)
-      |> to(:hls)
+      |> get_child(:video_nal_parser)
+      |> get_child(:video_payloader)
+      |> get_child(:video_cmaf_muxer)
+      |> via_in(:input, options: [
+        encoding: :H264,
+        segment_duration: %Membrane.HTTPAdaptiveStream.Sink.SegmentDuration{min: 4, target: 5}
+      ])
+      |> get_child(:hls)
     ]
 
-    spec = %ParentSpec{children: children, links: links}
+#    children = %{
+#      video_nal_parser: %Membrane.H264.FFmpeg.Parser{
+#        sps: state.video.sps,
+#        pps: state.video.pps,
+#        skip_until_keyframe?: true,
+#        skip_until_parameters?: false,
+#        framerate: {30, 1},
+#        alignment: :au,
+#        attach_nalus?: true
+#      },
+#      video_payloader: Membrane.MP4.Payloader.H264,
+#      video_cmaf_muxer: Membrane.MP4.Muxer.CMAF
+#    }
+
+#    links = [
+#      link(:rtp)
+#      |> via_out(Pad.ref(:output, ssrc),
+#        options: [depayloader: Membrane.RTP.H264.Depayloader]
+#      )
+#      |> to(:video_nal_parser)
+#      |> to(:video_payloader)
+#      |> to(:video_cmaf_muxer)
+#      |> via_in(:input)
+#      |> to(:hls)
+#    ]
+
+    spec = structure
 
     actions =
       if Map.has_key?(state, :rtp_started) do
@@ -106,31 +154,38 @@ defmodule Membrane.Demo.RtspToHls.Pipeline do
         [spec: spec]
       end
 
-    {{:ok, actions}, Map.put(state, :rtp_started, true)}
+    {actions, Map.put(state, :rtp_started, true)}
   end
 
   @impl true
-  def handle_notification({:new_rtp_stream, ssrc, _payload_type, _list}, :rtp, _ctx, state) do
+  def handle_child_notification({:new_rtp_stream, ssrc, _payload_type, _list}, :rtp, _ctx, state) do
     Logger.warn("new_rtp_stream Unsupported stream connected")
 
-    children = [
-      {{:fake_sink, ssrc}, Membrane.Element.Fake.Sink.Buffers}
-    ]
-
-    links = [
-      link(:rtp)
+    structure = [
+      child({:fake_sink, ssrc}, Membrane.Element.Fake.Sink.Buffers),
+      get_child(:rtp)
       |> via_out(Pad.ref(:output, ssrc))
-      |> to({:fake_sink, ssrc})
+      |> get_child({:fake_sink, ssrc})
     ]
 
-    spec = %ParentSpec{children: children, links: links}
-    {{:ok, spec: spec}, state}
+#    children = [
+#      {{:fake_sink, ssrc}, Membrane.Element.Fake.Sink.Buffers}
+#    ]
+
+#    links = [
+#      link(:rtp)
+#      |> via_out(Pad.ref(:output, ssrc))
+#      |> to({:fake_sink, ssrc})
+#    ]
+
+#    spec = %ParentSpec{children: children, links: links}
+    {[spec: structure], state}
   end
 
   @impl true
-  def handle_notification(notification, element, _ctx, state) do
+  def handle_child_notification(notification, element, _ctx, state) do
     Logger.warn("Unknown notification: #{inspect(notification)}, el: #{element}")
 
-    {:ok, state}
+    {[], state}
   end
 end

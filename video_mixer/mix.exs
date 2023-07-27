2d45ff10600c0fac84a5714fea198ea1d130fed7
varsill <lukasz.kita0@gmail.com>
2023-07-27 17:09:09 +0200

    Bump video mixer

--- video_mixer/mix.exs
+++ video_mixer/mix.exs
@@ -28,13 +28,13 @@ defmodule Membrane.Demo.VideoMixer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:membrane_core, "~> 0.7.0"},
-      {:membrane_file_plugin, "~> 0.6.0"},
-      {:membrane_video_merger_plugin, "~> 0.1.0"},
-      {:membrane_wav_plugin, "~> 0.1.0"},
-      {:membrane_h264_ffmpeg_plugin, "~> 0.10.0"},
-      {:membrane_aac_fdk_plugin, "~> 0.6.1"},
-      {:membrane_audio_mixer_plugin, "~> 0.1.0"},
+      {:membrane_core, "~> 0.12.7"},
+      {:membrane_file_plugin, "~> 0.14.0"},
+      {:membrane_video_merger_plugin, "~> 0.8.0"},
+      {:membrane_wav_plugin, "~> 0.9.1"},
+      {:membrane_h264_ffmpeg_plugin, "~> 0.27.0"},
+      {:membrane_aac_fdk_plugin, "~> 0.15.1"},
+      {:membrane_audio_mix_plugin, "~> 0.15.2"},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end

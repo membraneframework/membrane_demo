757bade3ce3aedac4b8e925b3f443709661c5f22
Bartek Chaliński <b.chalinski@gmail.com>
2024-04-17 03:28:05 +0200

    h264 -> h26x

--- rtp/mix.exs
+++ rtp/mix.exs
@@ -27,7 +27,7 @@ defmodule Membrane.Demo.RTP.MixProject do
       {:membrane_rtp_plugin, "~> 0.27.1"},
       {:membrane_udp_plugin, "~> 0.13.0"},
       {:membrane_h264_ffmpeg_plugin, "~> 0.31.5"},
-      {:membrane_h264_plugin, "~> 0.9.0"}, # update to h26x
+      {:membrane_h26x_plugin, "~> 0.10.1"},
       {:membrane_rtp_h264_plugin, "~> 0.19.0"},
       {:membrane_opus_plugin, "~> 0.20.1"},
       {:membrane_rtp_opus_plugin, "~> 0.9.0"},

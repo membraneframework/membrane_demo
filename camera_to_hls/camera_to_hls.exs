8bf688cb78a097045c339b89ce734ed83dea3187
Bartek Chaliński <b.chalinski@gmail.com>
2024-04-17 03:12:55 +0200

    camera_to_hls updated

--- camera_to_hls/camera_to_hls.exs
+++ camera_to_hls/camera_to_hls.exs
@@ -2,11 +2,11 @@ require Logger
 Logger.configure(level: :info)
 
 Mix.install([
-  {:membrane_core, "~> 1.0"},
-  {:membrane_camera_capture_plugin, "~> 0.7.1"},
-  {:membrane_ffmpeg_swscale_plugin, "~> 0.15.0"},
-  {:membrane_h264_ffmpeg_plugin, "~> 0.31.0"},
-  {:membrane_http_adaptive_stream_plugin, "~> 0.18.0"}
+  {:membrane_core, "~> 1.0.1"},
+  {:membrane_camera_capture_plugin, "~> 0.7.2"},
+  {:membrane_ffmpeg_swscale_plugin, "~> 0.15.1"},
+  {:membrane_h264_ffmpeg_plugin, "~> 0.31.5"},
+  {:membrane_http_adaptive_stream_plugin, "~> 0.18.4"}
 ])
 
 defmodule CameraToHls do

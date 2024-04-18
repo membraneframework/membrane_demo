f6cb6967d15339ae051f4f63f39bbbaa07c098c2
noarkhh <jakubpryc@student.agh.edu.pl>
2024-04-18 22:46:01 +0200

    Remove commented code:

--- rtsp_to_hls/lib/pipeline.ex
+++ rtsp_to_hls/lib/pipeline.ex
@@ -6,16 +6,13 @@ defmodule Membrane.Demo.RtspToHls.Pipeline do
 
   require Logger
 
-  alias Membrane.Demo.RtspToHls.ConnectionManager
-  alias Membrane.Pad
-
   @impl true
   def handle_init(_context, options) do
     Logger.debug("Source handle_init options: #{inspect(options)}")
 
     spec = [
       child(:source, %Membrane.RTSP.Source{
-        transport: :tcp,
+        transport: {:udp, options.port, options.port + 5},
         allowed_media_types: [:video],
         stream_uri: options.stream_url
       }),
@@ -31,74 +28,16 @@ defmodule Membrane.Demo.RtspToHls.Pipeline do
       )
     ]
 
-    # connection_manager_spec = [
-    #   %{
-    #     id: "ConnectionManager",
-    #     start:
-    #       {ConnectionManager, :start_link,
-    #        [
-    #          [
-    #            stream_url: options.stream_url,
-    #            port: options.port,
-    #            pipeline: self()
-    #          ]
-    #        ]},
-    #     restart: :transient
-    #   }
-    # ]
-
-    # Supervisor.start_link(connection_manager_spec,
-    #   strategy: :one_for_one,
-    #   name: Membrane.Demo.RtspToHls.Supervisor
-    # )
-
     {[spec: spec],
      %{
        video: nil,
-       port: options.port,
        output_path: options.output_path,
        parent_pid: options.parent_pid
      }}
-
-    # {[spec: spec], options}
   end
 
-  # @impl true
-  # def handle_info({:rtsp_setup_complete, options}, _ctx, state) do
-  #   Logger.debug("Source received pipeline options: #{inspect(options)}")
-
-  #   structure = [
-  #     child(
-  #       :app_source,
-  #       %Membrane.UDP.Source{
-  #         local_port_no: state[:port],
-  #         recv_buffer_size: 500_000
-  #       }
-  #     )
-  #     |> via_in(:rtp_input)
-  #     |> child(
-  #       :rtp,
-  #       %Membrane.RTP.SessionBin{
-  #         fmt_mapping: %{96 => {:H264, 90_000}}
-  #       }
-  #     ),
-  #     child(
-  #       :hls,
-  #       %Membrane.HTTPAdaptiveStream.SinkBin{
-  #         target_window_duration: Membrane.Time.seconds(120),
-  #         manifest_module: Membrane.HTTPAdaptiveStream.HLS,
-  #         storage: %Membrane.HTTPAdaptiveStream.Storages.FileStorage{
-  #           directory: state[:output_path]
-  #         }
-  #       }
-  #     )
-  #   ]
-
-  #   {[spec: structure], %{state | video: %{sps: options[:sps], pps: options[:pps]}}}
-  # end
-
   @impl true
-  def handle_child_notification({:new_track, ssrc, track}, :source, _ctx, state) do
+  def handle_child_notification({:new_track, ssrc, %{type: :video} = track}, :source, _ctx, state) do
     Logger.debug(":new_rtp_stream")
     IO.inspect(track, label: "track")
 
@@ -108,27 +47,9 @@ defmodule Membrane.Demo.RtspToHls.Pipeline do
         parameter_sets -> {parameter_sets.sps, parameter_sets.pps}
       end
 
-    # spss =
-    #   case state.video.sps do
-    #     <<>> -> []
-    #     sps -> [sps]
-    #   end
-
-    # ppss =
-    #   case state.video.pps do
-    #     <<>> -> []
-    #     pps -> [pps]
-    #   end
-    # spss = []
-    # ppss = []
-
     structure =
       get_child(:source)
-      |> via_out(
-        Pad.ref(:output, ssrc)
-        # options: [depayloader: Membrane.RTP.H264.Depayloader]
-      )
-      # |> child(:depayloader, Membrane.RTP.H264.Depayloader)
+      |> via_out(Pad.ref(:output, ssrc))
       |> child(
         :video_nal_parser,
         %Membrane.H264.Parser{

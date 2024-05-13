0c5e2307723a8ccd369a9ca4caa1060e80296675
Bartek Chaliński <b.chalinski@gmail.com>
2024-05-13 20:47:11 +0200

    formatting fix

--- rtp/lib/membrane_demo_rtp/send_pipeline.ex
+++ rtp/lib/membrane_demo_rtp/send_pipeline.ex
@@ -41,7 +41,11 @@ defmodule Membrane.Demo.RTP.SendPipeline do
       child(:audio_src, %File.Source{
         location: "samples/audio.opus"
       })
-      |> child(:audio_parser, %Opus.Parser{input_delimitted?: true, delimitation: :undelimit, generate_best_effort_timestamps?: true })
+      |> child(:audio_parser, %Opus.Parser{
+        input_delimitted?: true,
+        delimitation: :undelimit,
+        generate_best_effort_timestamps?: true
+      })
       |> via_in(Pad.ref(:input, audio_ssrc), options: [payloader: RTP.Opus.Payloader])
       |> get_child(:rtp)
       |> via_out(Pad.ref(:rtp_output, audio_ssrc), options: [encoding: :OPUS])

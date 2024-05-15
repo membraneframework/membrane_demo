4e23c78e2d6f2ace018cc52cef0b4297e5559eb9
Bartek Chaliński <b.chalinski@gmail.com>
2024-05-15 14:42:07 +0200

    bump swresample plugin

--- simple_pipeline/mix.exs
+++ simple_pipeline/mix.exs
@@ -31,7 +31,7 @@ defmodule Membrane.Demo.SimplePipeline.MixProject do
       {:membrane_core, "~> 1.0.1"},
       {:membrane_file_plugin, "~> 0.17.0"},
       {:membrane_portaudio_plugin, "~> 0.19.2"},
-      {:membrane_ffmpeg_swresample_plugin, "~> 0.19.1"},
+      {:membrane_ffmpeg_swresample_plugin, "~> 0.20.2"},
       {:membrane_mp3_mad_plugin, "~> 0.18.3"},
       {:dialyxir, "~> 1.2", only: [:dev], runtime: false}
     ]

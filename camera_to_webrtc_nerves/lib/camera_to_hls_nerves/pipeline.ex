735ac3fd38bba4678b2a2f6d3b50024164d87feb
noarkhh <jakub.pryc@gmail.com>
2024-10-03 14:46:50 +0200

    WIP

--- camera_to_webrtc_nerves/lib/camera_to_hls_nerves/pipeline.ex
+++ camera_to_webrtc_nerves/lib/camera_to_hls_nerves/pipeline.ex
@@ -1,21 +1,23 @@
 defmodule CameraToHlsNerves.Pipeline do
   use Membrane.Pipeline
 
-  alias Membrane.WebRTC
-
   def start_link(port) do
     Membrane.Pipeline.start_link(__MODULE__, port: port)
   end
 
   @impl true
   def handle_init(_ctx, opts) do
-    # Process.sleep(10_000)
+    Process.sleep(10_000)
 
     spec = [
       child(:source, Membrane.Rpicam.Source)
       |> child(:parser, Membrane.H264.Parser)
       |> via_in(Pad.ref(:input, :video_track), options: [kind: :video])
-      |> child(:webrtc, %WebRTC.Sink{signaling: {:websocket, port: opts[:port]}})
+      |> child(:webrtc, %Membrane.WebRTC.Sink{
+        signaling: {:websocket, port: opts[:port], ip: {0, 0, 0, 0}},
+        video_codec: :h264,
+        tracks: [:video]
+      })
     ]
 
     {[spec: spec], %{}}

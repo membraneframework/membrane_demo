008908a057f9a1584ed3779bdcc8493ec7f337b6
ty <lastcanal@gmail.com>
2024-11-05 15:50:13 -0500

    use defdelegate in client handler

--- rtmp_to_adaptive_hls/lib/rtmp_to_adaptive_hls/client_handler.ex
+++ rtmp_to_adaptive_hls/lib/rtmp_to_adaptive_hls/client_handler.ex
@@ -1,65 +1,36 @@
 defmodule Membrane.Demo.RtmpToAdaptiveHls.ClientHandler do
   @moduledoc """
-  An implementation of `Membrane.RTMPServer.ClienHandlerBehaviour` compatible with the
-  `Membrane.RTMP.Source` element.
+  An implementation of `Membrane.RTMPServer.ClientHandlerBehaviour` compatible with the
+  `Membrane.RTMP.Source` element, which also send information about RTMP stream metadata to the `pipeline` process
   """
 
   @behaviour Membrane.RTMPServer.ClientHandler
 
+  @handler Membrane.RTMP.Source.ClientHandlerImpl
+
   defstruct []
 
   @impl true
-  def handle_init(%{pipeline: pid}) do
-    %{
-      source_pid: nil,
-      buffered: [],
-      pipeline: pid
-    }
+  def handle_init(%{pipeline: pid} = opts) do
+    state = @handler.handle_init(opts)
+    Map.put(state, :pipeline, pid)
   end
 
   @impl true
-  def handle_info({:send_me_data, source_pid}, state) do
-    buffers_to_send = Enum.reverse(state.buffered)
-    state = %{state | source_pid: source_pid, buffered: []}
-    Enum.each(buffers_to_send, fn buffer -> send_data(state.source_pid, buffer) end)
-    state
-  end
+  defdelegate handle_info(msg, state), to: @handler
 
   @impl true
-  def handle_info(_other, state) do
-    state
-  end
+  defdelegate handle_data_available(payload, state), to: @handler
 
   @impl true
-  def handle_data_available(payload, state) do
-    if state.source_pid do
-      :ok = send_data(state.source_pid, payload)
-      state
-    else
-      %{state | buffered: [payload | state.buffered]}
-    end
-  end
+  defdelegate handle_connection_closed(state), to: @handler
 
   @impl true
-  def handle_connection_closed(state) do
-    if state.source_pid != nil, do: send(state.source_pid, :connection_closed)
-    state
-  end
-
-  @impl true
-  def handle_delete_stream(state) do
-    if state.source_pid != nil, do: send(state.source_pid, :delete_stream)
-    state
-  end
+  defdelegate handle_delete_stream(state), to: @handler
 
   @impl true
   def handle_metadata(message, state) do
     send(state.pipeline, message)
     state
-  end
-
-  defp send_data(pid, payload) do
-    send(pid, {:data, payload})
-    :ok
   end
 end

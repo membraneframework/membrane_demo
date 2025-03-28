20e0a17b3f290592f23de09ec4da2280c1efbff7
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2025-03-28 14:21:09 +0100

    Generate new Phoenix project

---
+++ webrtc_live_view/mix.exs
@@ -0,0 +1,67 @@
+defmodule WebrtcLiveView.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :webrtc_live_view,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
+      deps: deps()
+    ]
+  end
+
+  # Configuration for the OTP application.
+  #
+  # Type `mix help compile.app` for more information.
+  def application do
+    [
+      mod: {WebrtcLiveView.Application, []},
+      extra_applications: [:logger, :runtime_tools]
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Specifies your project dependencies.
+  #
+  # Type `mix help deps` for examples and options.
+  defp deps do
+    [
+      {:phoenix, "~> 1.7.21"},
+      {:phoenix_html, "~> 4.1"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_view, "~> 1.0"},
+      {:floki, ">= 0.30.0", only: :test},
+      {:phoenix_live_dashboard, "~> 0.8.3"},
+      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
+      {:telemetry_metrics, "~> 1.0"},
+      {:telemetry_poller, "~> 1.0"},
+      {:jason, "~> 1.2"},
+      {:dns_cluster, "~> 0.1.1"},
+      {:bandit, "~> 1.5"}
+    ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to install project dependencies and perform other setup tasks, run:
+  #
+  #     $ mix setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      setup: ["deps.get", "assets.setup", "assets.build"],
+      "assets.setup": ["esbuild.install --if-missing"],
+      "assets.build": ["esbuild webrtc_live_view"],
+      "assets.deploy": [
+        "esbuild webrtc_live_view --minify",
+        "phx.digest"
+      ]
+    ]
+  end
+end
---
+++ webrtc_live_view/test/webrtc_live_view_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule WebrtcLiveViewWeb.ErrorJSONTest do
+  use WebrtcLiveViewWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert WebrtcLiveViewWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert WebrtcLiveViewWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
